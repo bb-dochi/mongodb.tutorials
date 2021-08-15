f015673113a041233db4146b935238672350a3c8
gusrud2526 <gusrud2526@naver.com>
2021-08-15 18:11:30 +0900

    add env

--- MONGODB/src/server.js
+++ MONGODB/src/server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
 const { userRouter, blogRouter } = require("./routes");
-
 const mongoose = require("mongoose");
-const config = require("../config");
-const { generateFakeData } = require("../faker2");
 
 const server = async () => {
     try {
-        await mongoose.connect(config.MONGO_URI, {
+        const { MONGO_URI } = process.env;
+        if (!MONGO_URI) throw new Error("MONGO_URI is required!!");
+
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
             useFindAndModify: false,
         });
-        mongoose.set("debug", true);
+        // mongoose.set("debug", true);
         console.log("MongoDB connected");
 
         app.use(express.json());
@@ -22,8 +22,6 @@ const server = async () => {
         app.use("/blog", blogRouter);
 
         app.listen(3000, () => console.log("server listening on port 3000"));
-
-        // await generateFakeData(3, 3, 3);
     } catch (err) {
         console.log(err);
     }
